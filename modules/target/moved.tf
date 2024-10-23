55b1b3334271a45cd6e86a12c730573ab9563be9
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2024-10-23 18:47:16 +0200

    use access logs bucket name if passed

--- modules/target/moved.tf
+++ modules/target/moved.tf
@@ -2,3 +2,8 @@ moved {
   from = module.targets_bucket
   to   = module.target_bucket
 }
+
+moved {
+  from = module.target_bucket_access_logs_bucket
+  to   = module.target_bucket_access_logs_bucket[0]
+}
